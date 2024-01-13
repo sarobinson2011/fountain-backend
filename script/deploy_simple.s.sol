2a49269dfcc5eeffff9b6f5f812480c7acee329f
sarobinson2011 <sarobinson2011@gmail.com>
2024-01-13 10:01:51 +0000

    update simple.sol to include constructor arguments

--- script/deploy_simple.s.sol
+++ script/deploy_simple.s.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.22;
 
+//
+// Learn to deploy via this script (on top of 'forge create')
+//
+
 import "forge-std/console.sol";
 import {Script, console2} from "forge-std/Script.sol";
 import {SimpleToken} from "../src/simple.sol";
@@ -8,24 +12,30 @@ import {SimpleToken} from "../src/simple.sol";
 contract DeploySimple is Script {
     
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-        new SimpleToken();
+        uint256 privateKey = vm.envUint("PRIVATE_KEY_0x");
+        address account = vm.addr(privateKey);
+
+        console.log("Account:", account);
+
+        vm.startBroadcast(privateKey);
+        // deploy token
+        new SimpleToken("STOKEN", "STK");
+        // mint ?
         vm.stopBroadcast();
     }
 }
 
 // source .env
 
+// forge script script/deploy_simple.s.sol:DeploySimple
 
-// forge create --rpc-url INSERT_RPC_API_ENDPOINT \
-// --constructor-args 100 \
-// --private-key INSERT_YOUR_PRIVATE_KEY \
-// src/MyToken.sol:MyToken
-
+    // forge create --rpc-url INSERT_RPC_API_ENDPOINT \
+    // --constructor-args 100 \
+    // --private-key INSERT_YOUR_PRIVATE_KEY \
+    // src/MyToken.sol:MyToken
 
 // forge create src/simple.sol:SimpleToken --private-key $PRIVATE_KEY --rpc-url $SEPOLIA_RPC_URL --verify      <-- success
-
+//
 // forge create src/simple.sol:SimpleToken --private-key $PRIVATE_KEY --rpc-url $AURORA_TEST_RPC_URL           <-- fail
 // forge create src/simple.sol:SimpleToken --private-key $PRIVATE_KEY --rpc-url $AURORA_TEST_RPC_URL --legacy  <-- success
 //      --legacy --verify       <-- fail
@@ -41,3 +51,14 @@ contract DeploySimple is Script {
 // --verify \
 // src/simple.sol:SimpleToken
 
+
+
+
+// forge flatten --output src/simple.flattened.sol src/simple.sol
+
+// forge verify-contract --chain-id 1313161555 --num-of-optimizations 1000000 --watch \
+//     --compiler-version v0.8.22+commit.4fc1097e \
+//     <the_contract_address> \
+//     src/simple.sol:SimpleToken 
+
+// forge verify-contract --chain-id 1313161555 --num-of-optimizations 1000000 --watch --compiler-version v0.8.22+commit.4fc1097e 0xB864c40A6F9d20B9500f14f239862899ea8aC772 src/simple.sol:SimpleToken
--- src/simple.sol
+++ src/simple.sol
@@ -7,23 +7,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
     Simple contract to test contract verification
 */
 
-contract SimpleToken is ERC20 {
-    address public _owner;
+// contract SimpleToken is ERC20 {
+//     address public _owner;
     
-    constructor() ERC20("MyToken", "MTK") {
-        _owner = msg.sender;
+//     constructor() ERC20("MyToken", "MTK") {
+//         _owner = msg.sender;
+//     }
+    
+// }
+
+contract SimpleToken is ERC20 {
+    address public owner;
+    
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        owner = msg.sender;
     }
     
 }
 
-// forge flatten --output src/simple.flattened.sol src/simple.sol
-
-// forge verify-contract --chain-id 1313161555 --num-of-optimizations 1000000 --watch \
-//     --compiler-version v0.8.22+commit.4fc1097e \
-//     <the_contract_address> \
-//     src/simple.sol:SimpleToken 
-
-// forge verify-contract --chain-id 1313161555 --num-of-optimizations 1000000 --watch --compiler-version v0.8.22+commit.4fc1097e 0xB864c40A6F9d20B9500f14f239862899ea8aC772 src/simple.sol:SimpleToken
 
 
 
