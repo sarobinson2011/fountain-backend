903ab3f02527455cb95fd348d4e799654a1393b7
sarobinson2011 <sarobinson2011@gmail.com>
2024-01-29 16:47:07 +0000

    deploy and verify on Arbitrum Sepolia working now

--- script/use_lockdrop.s.sol
+++ script/use_lockdrop.s.sol
@@ -29,3 +29,6 @@ contract DeploySimple is Script {
 
 // Arbitrum Sepolia
 // forge verify-contract --chain-id 421614 --num-of-optimizations 1000000 --watch --compiler-version v0.8.19+commit.4fc1097e 0x5912a392035116C62A40a400e3F54E4523227420 src/lockdrop.sol:LockDrop
+
+
+// forge verify-contract --chain-id 421614 --num-of-optimizations 1000000 --watch --compiler-version v0.8.22+commit.4fc1097e 0xD860631A3957656cE86E9227Ae1E6C54763EB757 src/simple.sol:SimpleToken --etherscan-api-key ${ARBISCAN_API_KEY} --verifier-url ${ARBITRUM_SEPOLIA_API}
