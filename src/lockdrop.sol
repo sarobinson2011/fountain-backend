8afab3597fc8c244f22782d7cc95bf6a618e52b4
sarobinson2011 <sarobinson2011@gmail.com>
2024-01-31 19:31:15 +0000

    update lockdrop.sol

--- src/lockdrop.sol
+++ src/lockdrop.sol
@@ -10,12 +10,13 @@ contract LockDrop {
     struct TimedDeposit {
         uint256 amount;
         uint256 timestamp;
-        uint256 reward;         // need logic to calculate the reward distribution
+        // uint256 reward;         // need logic to calculate the reward distribution
     }
 
     mapping (address => TimedDeposit) public balances;   
     event NewDeposit(address indexed _user, uint256 _amount);
     event NewWithdraw(address indexed _user, uint256 _amount);
+    event RewardsTransferred(address indexed _user, uint256 _amount);
 
     constructor() {
         owner = msg.sender;
@@ -26,31 +27,41 @@ contract LockDrop {
         balances[msg.sender] = TimedDeposit(
             {
                 amount: msg.value, 
-                timestamp: block.timestamp,
-                reward: 0                       // zero upon deposit
+                timestamp: block.timestamp
+                // reward: 0                       // zero upon deposit
             }
         );
         emit NewDeposit(msg.sender, msg.value);
     } 
     
     function withdraw() external {
+        uint256 reward = calculateReward(); //    <-- returns the reward value to transfer
         require(balances[msg.sender].amount > 0, "You have no balance to withdraw...");
         require(block.timestamp >= balances[msg.sender].timestamp + 5 minutes, "Time lock not expired...");
-        
-        // Calculate reward as 10% of the deposited amount
-        // uint256 reward = balances[msg.sender].amount / 10;
-        
+              
         uint256 tempAmount = balances[msg.sender].amount;
         balances[msg.sender].amount = 0;
         balances[msg.sender].timestamp = 0;
 
-        // Transfer the tokens to the withdrawer
-        // require(token.transfer(msg.sender, reward), "Token transfer failed");
-
-        balances[msg.sender].reward = 0;
+        // Transfer RWDZ tokens to the withdrawer
+        require(tokenReward.transfer(msg.sender, reward), "RWDZ token transfer failed");
+        emit RewardsTransferred(msg.sender, reward);
+        reward = 0;
+                
+        // Transfer deposited Ether to the withdrawer
         payable(msg.sender).transfer(tempAmount);
-        tempAmount = 0;
         emit NewWithdraw(msg.sender, tempAmount);
+        tempAmount = 0;
+
+    }
+
+    function calculateReward() internal returns (uint256) {
+
+        // need to calculate reward for balances[msg.sender]
+
+        // uint256 reward = balances[msg.sender].amount / 10;
+        
+        // return reward;       <-- return value to withdraw()
     }
 } 
 
