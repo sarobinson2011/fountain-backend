8d349f28cb216cfb6352847a4f3f4f6ea378c604
sarobinson2011 <sarobinson2011@gmail.com>
2024-04-07 17:49:36 +0100

    update sandbox - initial design for the chainlink randomness event listener

--- src/sandbox.sol
+++ src/sandbox.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {VRFv2Consumer} from "../src/randomNumberGenerator.sol";
+
+
 contract EventListener {
     // blah
+    VRFv2Consumer public vrfv2consumer;
+
+    constructor(address _vrfConsumerAddress) {
+        vrfv2consumer = VRFv2Consumer(_vrfConsumerAddress);
+    }
+
+    function listenToEvent() external {
+        // Subscribe to the event
+        VRFv2Consumer(vrfv2consumer).RequestFulfilled(0, new uint256 );     // <-- HERE !!
+    }
+
 }
 
 
